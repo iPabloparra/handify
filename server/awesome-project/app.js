99da93d335c18f36fc04ad2458b9ad6249596d4f
Pablo Parra <pabloparramolina@gmail.com>
2017-05-27 19:48:48 +0200

    enought for today

--- server/awesome-project/app.js
+++ server/awesome-project/app.js
@@ -40,15 +40,11 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -61,7 +57,6 @@ app.use('/', index);
 app.use('/', incidences);
 app.use('/', services);
 
-// Passport config
 app.use(session({
   secret: "passport-local-strategy",
   resave: true,
@@ -72,26 +67,18 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-//app.use('/', index);
-
 app.use('/', authController);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
