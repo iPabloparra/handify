cdc77b5c2de10d1e2b1222ee9b41f9299b371dc3
Pablo Parra <pabloparramolina@gmail.com>
2017-05-27 19:52:31 +0200

    removed useless comments

--- server/awesome-project/routes/users.js
+++ server/awesome-project/routes/users.js
@@ -1,14 +1,11 @@
-const express        = require("express");
+const express = require("express");
 const users = express.Router();
 const passport = require("passport");
 
-// Our user model
-const User           = require("../models/User");
+const User = require("../models/User");
 const upload = require('../config/multer');
 
-// Bcrypt let us encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
-
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 module.exports = users;
