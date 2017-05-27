5822a17b6a7ba9d939e3e1d5a4771df346ce6e06
Pablo Parra <pabloparramolina@gmail.com>
2017-05-27 19:53:08 +0200

    he escrito un libro...

--- server/awesome-project/routes/services.js
+++ server/awesome-project/routes/services.js
@@ -1,37 +1,37 @@
-const express        = require("express");
+const express = require("express");
 const serviceRoute = express.Router();
 const passport = require("passport");
 
-// Our user model
-const Service           = require("../models/Service");
+const Service = require("../models/Service");
 const upload = require('../config/multer');
 
-// Bcrypt let us encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 serviceRoute.post("/create-service", (req, res, next) => {
-  console.log(req.body)
-  var userId = req.body.userId;
-  var price = req.body.price;
-  var category = req.body.category;
-  var available = req.body.available
-  var newService = Service({
-      userId,
-      price,
-      category,
-      available,
+    console.log(req.body)
+    var userId = req.body.userId;
+    var price = req.body.price;
+    var category = req.body.category;
+    var available = req.body.available
+    var newService = Service({
+        userId,
+        price,
+        category,
+        available,
     });
 
     newService.save((err) => {
-      if (err) {
+        if (err) {
 
-        res.status(400).json({ message: "Something went wrong" });
-      } else {
-          res.status(200).json(req.body);
+            res.status(400).json({
+                message: "Something went wrong"
+            });
+        } else {
+            res.status(200).json(req.body);
         };
-      });
     });
+});
 
 
 
